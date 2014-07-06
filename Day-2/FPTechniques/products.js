2b5f9351315abb44eca414ae0749321f7729e4a3
tkmagesh <tkmagesh77@gmail.com>
2014-07-06 13:33:16 +0530

    Model View Separation

--- Day-2/FPTechniques/products.js
+++ Day-2/FPTechniques/products.js
@@ -67,3 +67,158 @@ function productValueComparer(p1,p2){
 console.log("Sorting using Comparer function for product value...")
 sort(products,productValueComparer);
 console.table(products);
+
+/*
+filter
+min
+
+groupBy
+join
+*/
+
+var filter = function (list){
+	var result = [];
+	for(var i=0;i<list.length;i++)
+		if (list[i].cost > 50)
+			result.push(list[i]);
+	return result;
+}
+
+var costlyProducts = filter(products);
+console.log("All costly products")
+console.table(costlyProducts);
+
+var filter = function (list, predicate){
+	var result = [];
+	for(var i=0;i<list.length;i++)
+		if (predicate(list[i]))
+			result.push(list[i]);
+	return result;
+}
+var cheapProductCriteria = function(p){
+	return p.cost <= 50;
+}
+var cheapProducts = filter(products,cheapProductCriteria);
+console.log("All cheap products")
+console.table(cheapProducts);
+
+console.log("Min using attribute name")
+var min = function(list,attrName){
+	var result = list[0][attrName];
+	for(var i=1;i<list.length;i++)
+		if (list[i][attrName] < result)
+			result = list[i][attrName];
+	return result;
+}
+console.log("Min cost = ", min(products,"cost"));
+
+
+console.log("Min using selector function")
+var min = function(list,selectorFn){
+	var result = selectorFn(list[0]);
+	for(var i=1;i<list.length;i++){
+		var value = selectorFn(list[i])
+		if (value < result)
+			result = value;
+	}
+	return result;
+}
+var productValueSelectorFn = function(p){ return p.units * p.cost};
+console.log("Min product value = ", min(products,productValueSelectorFn));
+
+var min = function(list,selector){
+	var selectorFn = typeof selector === "function" ? selector : function(item){return item[selector]};
+	var result = selectorFn(list[0]);
+	for(var i=1;i<list.length;i++){
+		var value = selectorFn(list[i])
+		if (value < result)
+			result = value;
+	}
+	return result;
+}
+
+
+var groupBy = function(list,selector){
+	var selectorFn = typeof selector === "function" ? selector : function(item){return item[selector]};
+	var result = {};
+	for(var i=0;i<list.length;i++){
+		var key = selectorFn(list[i])
+		if (typeof result[key] === "undefined")
+			result[key] = [];
+		result[key].push(list[i])
+	}
+	return result;
+}
+var productsByCategory = groupBy(products,"category");
+
+var productCatergorizerByCost = function(p){
+	return p.cost <= 50 ? "affordable" : "costly";
+}
+var productsByCost = groupBy(products,productCatergorizerByCost);
+console.log("After grouping by cost")
+console.table(productsByCost.affordable)
+console.table(productsByCost.costly);
+
+var categories = [
+	{id : 1, name : "stationary"},
+	{id : 2, name : "grocery"}
+]
+
+var join = function(leftList, rightList, leftKeyAttrName, rightKeyAttrName, transformerFn){
+	var result = [];
+	for(var i=0;i<leftList.length;i++){
+		var leftItem = leftList[i],
+			leftKey = leftItem[leftKeyAttrName];
+		for(var j=0;j<rightList.length;j++){
+			var rightItem = rightList[j],
+				rightKey = rightItem[rightKeyAttrName];
+			if (leftKey === rightKey)
+				result.push(transformerFn(leftItem, rightItem));
+		}
+	}
+	return result;
+}
+
+console.log("joining products and categories")
+var productsWithCategoryNames = join(products,categories,"category","id",function(p,c){
+	return {id : p.id, name : p.name, cost : p.cost, units : p.units, category : c.name};
+});
+console.table(productsWithCategoryNames);
+
+function memoize(fn){
+	var cache = {};
+	return function(){
+		if (typeof cache[arguments[0]] !== "undefined") {
+			console.log("from cache");
+			return cache[arguments[0]];
+		}
+		var result = fn.apply(this,[].slice.call(arguments,0));
+		cache[arguments[0]] = result;
+		console.log("juz processed");
+		return result;
+	}
+}
+
+
+
+/*
+
+max (HW)
+sum (HW)
+avg (HW)
+countBy (HW - use predicates)
+all (HW - use predicates)
+any (HW - use predicates)
+
+each (underscore - Collections)
+map (underscore - Collections)
+reduce (underscore - Collections)
+reject (underscore - Collections)
+
+memoize (underscore - Functions)
+once (underscore - Functions)
+after (underscore - Functions)
+compose (underscore - Functions)
+
+Try solving the problems with using "for"
+*/
