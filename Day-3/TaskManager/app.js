55ede45741a77b9ccb95f4ef570739a104167696
tkmagesh <tkmagesh77@gmail.com>
2014-07-13 11:52:37 +0530

    Server app in node.js

---
+++ Day-3/TaskManager/app.js
@@ -0,0 +1,39 @@
+var http = require("http"),
+	fs = require("fs"),
+	url = require("url"),
+	path = require("path"),
+	querystring = require("querystring"),
+	knownFileExtenstions = [".html",".css",".img",".js"],
+	tasks = [
+		{name : "Task - 1", isCompleted : false, category : "Personal"},
+		{name : "Task - 2", isCompleted : false, category : "Official"},
+	];
+
+String.prototype.endsWith = function(extn){
+   return this.substr(this.length - extn.length, extn.length) === extn;
+}
+
+
+
+http.createServer(function(req,res){
+	var pathName = url.parse(req.url).pathname;
+	var resourceName = path.join(__dirname, pathName);
+	var isFile = knownFileExtenstions.some(function(ext){
+		return resourceName.endsWith(ext);
+	});
+	if (isFile){
+		if (fs.existsSync(resourceName)){
+			fs.createReadStream(resourceName, {encoding : "utf8"}).pipe(res);
+		} else {
+			res.statusCode = 404;
+			res.end();
+		}	
+	} else {
+		if (pathName === "/tasks"){
+			res.write(JSON.stringify(tasks));
+			res.end();
+		}
+	}
+	
+}).listen(8080);
+console.log("Server listening on port 8080..");
